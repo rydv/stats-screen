6bd4919ec63b7ce95b51f9fc912f3a6b8fd69853
Sanjay <sanjayrydv@gmail.com>
2024-10-03 00:38:14 +0530

    partname logic update

--- api-02/controller/modules/strategies/partname_strategy.py
+++ api-02/controller/modules/strategies/partname_strategy.py
@@ -12,42 +12,51 @@ class ClientNameStrategy(BaseStrategy):
         # Implement validation logic if needed
         pass
 
-    def build_query(self, filter_data):
-        query = {
-            "query": {
-                "bool": {
-                    "must": [
-                        {"term": {"COUNTRY": self.rule_params.category_code}}
-                    ],
-                    "should": [],
-                    "minimum_should_match": 1
-                }
-            }
-        }
+    def _get_combinations(self, group_index):
+        return [combo for i in range(2, len(group_index)+1) for combo in combinations(group_index, i)]
 
-        if self.rule_params.set_id != 'ALL':
-            query["query"]["bool"]["must"].append({"terms": {"LOCAL_ACC_NO": self.rule_params.set_id}})
-
-        tran_type = [f"{filter_data['ls_flag']} {filter_data['dc_flag']}R"] if filter_data['dc_flag'] != 'A' else [f"{filter_data['ls_flag']} CR", f"{filter_data['ls_flag']} DR"]
-        query["query"]["bool"]["must"].append({"terms": {"C_OR_D": tran_type}})
-
-        for op_item in filter_data['op_items']:
-            query["query"]["bool"]["should"].append({"regexp": {op_item['field_name']: op_item['search_agg_exp']}})
-
-        return query
-
-    def _extract_matched_value(self, transaction, op_items):
-        matched_values = []
+    def _get_matched_values(self, transaction, op_filters):
+        matched_values_list = []
         partname_values = []
-        for op_item in op_items:
-            if op_item['exp_flag'] or op_item['partname_flag']:
-                match = re.search(op_item['search_agg_exp'], transaction[op_item['field_name']])
-                if match:
-                    groups = match.groups()
-                    matched_values.extend(groups[:-1])  # All groups except the last one (partname)
-                    partname_values.append(groups[-1])  # Last group is partname
+
+        filter_id = transaction.get('filter_id')
+        if filter_id not in op_filters:
+            return [], ''
         
-        return " | ".join(matched_values), " ".join(partname_values)
+        for op_item in op_filters[filter_id]['op_items']:
+            field_value = transaction[op_item['field_name']]
+            matches = re.findall(op_item['search_agg_exp'], field_value)
+            
+            for match in matches:
+                if isinstance(match, tuple):
+                    if op_item['exp_flag'] or op_item['partname_flag']:
+                        matched_values_list.extend(match[:-1])
+                        partname_values.append(match[-1])
+                    else:
+                        matched_values_list.extend(match)
+                else:
+                    matched_values_list.append(match)
+            
+            if op_item['op_flag']:
+                op_params = op_item['op_params']
+                if op_params['op_type'] == 'REP':
+                    matched_values_list = [value.replace(op_params['s1'], op_params['s2']) for value in matched_values_list]
+        
+        matched_values_list = list(set(matched_values_list))
+        matched_values_list.sort()
+        partname = " ".join(set(partname_values))
+        return '/'.join(matched_values_list).strip().strip('|'), partname
+
+    def _process_relationship_group(self, group):
+        if len(set(group['filter_id'])) < len(self.op_filters) or not self.check_value_date(group) or not self.check_amount(group):
+            return False, None
+        
+        group['MATCHED_VALUES'], group['PARTNAME'] = zip(*group.apply(lambda transaction: self._get_matched_values(transaction, self.op_filters), axis=1))
+        
+        if group['MATCHED_VALUES'].nunique() == 1 and self._compare_partnames(group['PARTNAME'].iloc[0], group['PARTNAME'].iloc[1]):
+            return True, group
+        
+        return False, None
 
     def _compare_partnames(self, name1, name2):
         name1_parts = set(name1.lower().split())
@@ -59,52 +68,53 @@ class ClientNameStrategy(BaseStrategy):
         for filter_id, filter_data in self.op_filters.items():
             query = self.build_query(filter_data)
             scroll_id = None
+            filter_matches = []
             while True:
                 scroll_id, hits = self.scroll_transactions(query, scroll_id)
                 if not hits:
                     break
                 for match in hits:
                     match['filter_id'] = filter_id
-                all_matches.extend(hits)
+                filter_matches.extend(hits)
+            if not filter_matches:
+                return pd.DataFrame()
+            all_matches.extend(filter_matches)
 
         df = pd.DataFrame(all_matches)
         grouped = df.groupby('RELATIONSHIP_ID')
         final_matches = []
-
+        grp_count = 0
         for rel_id, group in grouped:
-            combinations_list = list(combinations(group.index, 2))
-            for i, combo in enumerate(combinations_list):
-                combo_df = group.loc[combo]
-                
-                if len(set(combo_df['filter_id'])) != 2:
-                    continue
-                
-                for idx, row in combo_df.iterrows():
-                    filter_data = self.op_filters[row['filter_id']]
-                    row['concat_matched_value'], row['partname'] = self._extract_matched_value(row, filter_data['op_items'])
-                    combo_df.loc[idx] = row
+            group = group.drop_duplicates(subset='ITEM_ID')
+            grp_count += 1
+            if len(group) <= 10:
+                combinations_list = self._get_combinations(group.index)
+                matched_item_ids = set()
+                for i, combo in enumerate(combinations_list):
+                    combo_df = group.loc[list(combo)]
+                    if any(item_id in matched_item_ids for item_id in combo_df['ITEM_ID']):
+                        continue
+                    out_flag, matched_group = self._process_relationship_group(combo_df)
+                    if out_flag:
+                        matched_group["MATRIX_RELATIONSHIP_ID"] = f"{self.rule_params.unique_rule_id}-{rel_id}-{grp_count}{i+1}"
+                        final_matches.append(matched_group)
+                        matched_item_ids.update(matched_group['ITEM_ID'].tolist())
+            elif self.rule_params.amount_check_flags['amount_flag'] == 'S' or self.rule_params.amount_check_flags['amount_flag'] == 'Same':
+                sorted_group = group.sort_values('AMOUNT')
+                amount_groups = sorted_group.groupby('AMOUNT')
+                ag_count = 0
+                for amount, amount_group in amount_groups:
+                    ag_count += 1
+                    out_flag, matched_group = self._process_relationship_group(amount_group)
+                    if out_flag:
+                        matched_group['MATRIX_RELATIONSHIP_ID'] = f"{self.rule_params.unique_rule_id}-{rel_id}-{grp_count}{ag_count}"
+                        final_matches.append(matched_group)
+            else:
+                pass
 
-                if self._compare_partnames(combo_df['partname'].iloc[0], combo_df['partname'].iloc[1]):
-                    if self.check_amount(combo_df):
-                        for _, row in combo_df.iterrows():
-                            match = row.to_dict()
-                            match['MATRIX_RELATIONSHIP_ID'] = f"{rel_id}_{i+1}"
-                            match['MATCHED_VALUES'] = f"{match['concat_matched_value']} | {match['partname']}"
-                            final_matches.append(match)
-
-        final_df = pd.DataFrame(final_matches)
+        final_df = pd.concat(final_matches).reset_index(drop=True)
         if len(final_df):
+            final_df["MATCHED_VALUES"] = final_df["MATCHED_VALUES"] + " | " + final_df["PARTNAME"]
+            del final_df['PARTNAME']
             final_df["Rule_Id"] = self.rule_params.unique_rule_id
-
         return final_df
-
-    def check_amount(self, df):
-        if self.rule_params.amount_check_flags:
-            for amount_field_flag, amount_flag_value in self.rule_params.amount_check_flags.items():
-                if not self.check_amount_flag_condition(amount_check_mapping[amount_field_flag], amount_flag_value, df):
-                    return False
-        return True
-
-    def check_amount_flag_condition(self, amount_field, amount_flag_value, group):
-        # Implement the amount flag condition check logic here
-        pass
